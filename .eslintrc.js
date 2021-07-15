02ccc2397dbfa34ef3d8be4600a98bb8df198083
J. Ryan Stinnett <jryans@gmail.com>
2021-07-15 12:52:04 +0100

    Add Preact linting
    This mostly uses the React linting toolchain, just adjusting the JSX pragma to
    match Preact. For now, I've copied rules in manually since I don't instead to
    use hooks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         "matrix-org",
+        "react",
     ],
     extends: [
         "plugin:matrix-org/typescript",
@@ -9,7 +10,37 @@ module.exports = {
         browser: true,
         node: true,
     },
+    settings: {
+        react: {
+            pragma: "h",
+        },
+    },
     rules: {
         "quotes": "off",
+
+        // React rules adapted from eslint-plugin-matrix-org
+        // Rules for hooks removed to avoid extra dependencies
+
+        "max-len": ["warn", {
+            // Ignore pure JSX lines
+            ignorePattern: '^\\s*<',
+            ignoreComments: true,
+            code: 120,
+        }],
+
+        // This just uses the React plugin to help ESLint known when
+        // variables have been used in JSX
+        "react/jsx-uses-vars": ["error"],
+        // Don't mark React as unused if we're using JSX
+        "react/jsx-uses-react": ["error"],
+
+        // Components in JSX should always be defined
+        "react/jsx-no-undef": ["error"],
+
+        // Assert spacing before self-closing JSX tags, and no spacing before
+        // or after the closing slash, and no spacing after the opening
+        // bracket of the opening tag or closing tag.
+        // https://github.com/yannickcr/eslint-plugin-react/blob/HEAD/docs/rules/jsx-tag-spacing.md
+        "react/jsx-tag-spacing": ["error"],
     },
 };
