e6edc16b65c2faca31fbd79501da2dd0a27f79ee
J. Ryan Stinnett <jryans@gmail.com>
2021-07-23 10:34:41 +0100

    Rework linting to avoid TS/JS rule overlap

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,61 +3,70 @@ module.exports = {
         "matrix-org",
         "react",
     ],
-    extends: [
-        "plugin:matrix-org/typescript",
-    ],
     env: {
+        es2020: true,
         browser: true,
         node: true,
     },
-    settings: {
-        react: {
-            pragma: "h",
-        },
+    parserOptions: {
+        sourceType: "module",
     },
-    rules: {
-        "quotes": ["error", "double"],
+    overrides: [
+        {
+            files: [
+                "**/*.ts",
+                "**/*.tsx",
+            ],
+            extends: [
+                "plugin:matrix-org/typescript",
+            ],
+            settings: {
+                react: {
+                    pragma: "h",
+                },
+            },
+            rules: {
+                "quotes": ["error", "double"],
 
-        // React rules adapted from eslint-plugin-matrix-org
-        // Rules for hooks removed to avoid extra dependencies
+                // React rules adapted from eslint-plugin-matrix-org
+                // Rules for hooks removed to avoid extra dependencies
 
-        "max-len": ["warn", {
-            // Ignore pure JSX lines
-            ignorePattern: "^\\s*<",
-            ignoreComments: true,
-            code: 120,
-        }],
+                "max-len": ["warn", {
+                    // Ignore pure JSX lines
+                    ignorePattern: "^\\s*<",
+                    ignoreComments: true,
+                    code: 120,
+                }],
 
-        // This just uses the React plugin to help ESLint known when
-        // variables have been used in JSX
-        "react/jsx-uses-vars": ["error"],
-        // Don't mark React as unused if we're using JSX
-        "react/jsx-uses-react": ["error"],
+                // This just uses the React plugin to help ESLint known when
+                // variables have been used in JSX
+                "react/jsx-uses-vars": ["error"],
+                // Don't mark React as unused if we're using JSX
+                "react/jsx-uses-react": ["error"],
 
-        // Components in JSX should always be defined
-        "react/jsx-no-undef": ["error"],
+                // Components in JSX should always be defined
+                "react/jsx-no-undef": ["error"],
 
-        // Assert spacing before self-closing JSX tags, and no spacing before
-        // or after the closing slash, and no spacing after the opening
-        // bracket of the opening tag or closing tag.
-        // https://github.com/yannickcr/eslint-plugin-react/blob/HEAD/docs/rules/jsx-tag-spacing.md
-        "react/jsx-tag-spacing": ["error"],
+                // Assert spacing before self-closing JSX tags, and no spacing before
+                // or after the closing slash, and no spacing after the opening
+                // bracket of the opening tag or closing tag.
+                // https://github.com/yannickcr/eslint-plugin-react/blob/HEAD/docs/rules/jsx-tag-spacing.md
+                "react/jsx-tag-spacing": ["error"],
 
-        // Empty interfaces are useful for declaring new names
-        "@typescript-eslint/no-empty-interface": ["off"],
-    },
-    overrides: [{
-        files: [
-            "**/*.js",
-        ],
-        extends: [
-            "plugin:matrix-org/javascript",
-        ],
-        rules: {
-            "quotes": ["error", "double"],
-
-            // Unsure why TS rules are being loaded at all...
-            "@typescript-eslint/no-var-requires": ["off"],
+                // Empty interfaces are useful for declaring new names
+                "@typescript-eslint/no-empty-interface": ["off"],
+            },
         },
-    }],
+        {
+            files: [
+                "**/*.js",
+            ],
+            extends: [
+                "plugin:matrix-org/javascript",
+            ],
+            rules: {
+                "quotes": ["error", "double"],
+            },
+        },
+    ],
 };
