d855ac6e4e2aa6a9fc4fb55a4b9d7054f7779957
J. Ryan Stinnett <jryans@gmail.com>
2021-10-13 14:39:17 +0100

    Add test files to Vite's optimisation entries
    This works around an issue where Vite might discover test dependencies (such as
    `chai`) mid-way through the first test run and try to reload the page, breaking
    the test run. By telling it about the test files up front, they are pre-bundling
    along with Patience before the test run begins.

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -2,18 +2,36 @@ import path from "path";
 
 import { defineConfig, searchForWorkspaceRoot } from "vite";
 
+const {
+    PATIENCE_TEST_DIR: testDir,
+    PATIENCE_TEST_FILES: testFiles,
+} = process.env;
+
 const fsAllow = [
     searchForWorkspaceRoot(process.cwd()),
 ];
 // Allow test directory if running tests via `patience` command
-if (process.env.PATIENCE_TEST_DIR) {
-    fsAllow.push(process.env.PATIENCE_TEST_DIR);
+if (testDir) {
+    fsAllow.push(testDir);
+}
+
+const optimizeDepsEntries = [
+    // Always optimise Patience itself, as Vite default would have done.
+    "index.html",
+];
+if (testDir && testFiles) {
+    // Add test files to Vite's optimisation entries to ensure they are
+    // processed the first time, avoiding Vite reloads that may break the test
+    // runner.
+    const rootDir = path.join(process.cwd(), "framework");
+    const frameworkRelativeTestDir = path.relative(rootDir, testDir);
+    optimizeDepsEntries.push(path.join(frameworkRelativeTestDir, testFiles));
 }
 
 let testPrefix = "";
-if (process.env.PATIENCE_TEST_DIR) {
+if (testDir) {
     // Tests are requested by their relative path with upward steps removed
-    const testRelPath = path.relative(process.cwd(), process.env.PATIENCE_TEST_DIR);
+    const testRelPath = path.relative(process.cwd(), testDir);
     testPrefix = "/" + testRelPath.replace(/\.\.[/\\]/g, "");
 }
 
@@ -46,12 +64,15 @@ export default defineConfig({
             },
         },
         // Only use HMR for development of Patience itself
-        hmr: !process.env.PATIENCE_TEST_DIR,
+        hmr: !testDir,
     },
     build: {
         outDir: "./build",
         minify: false,
         sourcemap: true,
+    },
+    optimizeDeps: {
+        entries: optimizeDepsEntries,
     },
     plugins: [
         {
@@ -59,7 +80,6 @@ export default defineConfig({
             enforce: "pre",
             resolveId(source) {
                 // Resolve test directory if running tests via `patience` command
-                const testDir = process.env.PATIENCE_TEST_DIR;
                 if (testDir) {
                     if (source.startsWith(testPrefix)) {
                         return source.replace(testPrefix, testDir);
